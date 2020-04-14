4246bacde1913d74fb2a72d65c742519f5c9ce87
Ville Kautto <ville.kautto@hotmail.fi>
2020-04-14 19:53:13 +0300

    add scene loader for moving between scenes
    added scene loader object which loads other scenes

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,12 @@
+﻿﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+ public class SceneLoader : MonoBehaviour
+{
+    
+  private void LoadLevel(int Level)
+  { 
+      SceneManager.LoadScene(Level);
+  }
+}
