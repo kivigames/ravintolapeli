2b86464f07df9cca05f8d8e145a87270385a8e3f
Aleksi Kervinen <me@aleksi.me>
2020-05-29 12:32:31 +0300

    add exit to back button/cancel

---
+++ Assets/Scripts/BackButtonExit.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(InputSystemUIInputModule))]
+public class BackButtonExit : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<InputSystemUIInputModule>().cancel.action.performed += Exit;
+    }
+
+    private void OnDestroy()
+    {
+        GetComponent<InputSystemUIInputModule>().cancel.action.performed -= Exit;
+    }
+
+    private void Exit(InputAction.CallbackContext context)
+    {
+        Debug.Log("Cancel pressed, quitting...");
+
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            // Main Menu
+            Application.Quit();
+        }
+        else
+        {
+            // Else return to main menu
+            SceneManager.LoadScene(0);
+        }
+    }
+}
